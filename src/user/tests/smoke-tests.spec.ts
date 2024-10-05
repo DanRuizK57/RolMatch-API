93ff8497a12e7de7652c4447e4adc535ee16e5c1
Daniel Ruiz Kemp <d.ruiz03@ufromail.cl>
2024-10-05 16:19:24 -0300

    (tests) Prueba de humo de findAll() finalizada

--- src/user/tests/smoke-tests.spec.ts
+++ src/user/tests/smoke-tests.spec.ts
@@ -5,6 +5,7 @@ import { AppModule } from '../../app.module';
 import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
 import { Repository } from 'typeorm';
+import { UserService } from '../user.service';
 
 /*
   Pruebas de humo para verificar el correcto funcionamiento del módulo de usuarios.
@@ -13,6 +14,13 @@ import { Repository } from 'typeorm';
 describe('Pruebas de humo para el módulo de usuario', () => {
   let app: INestApplication;
   let userRepository: Repository<User>;
+  let userService: UserService;
+
+  const mockedUsers = [
+        // Se asignan de esta manera para que los detecte como User y no como Object
+        Object.assign(new User(), { id: 1, firstName: 'John', lastName: "Doe", email: 'john.doe@example.com', picture: '', reports: 0 }),
+        Object.assign(new User(), { id: 2, firstName: 'Jane', lastName: "Doe", email: 'jane.doe@example.com', picture: '', reports: 0 }),
+    ];
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -21,11 +29,13 @@ describe('Pruebas de humo para el módulo de usuario', () => {
 
     app = moduleFixture.createNestApplication();
     userRepository = moduleFixture.get<Repository<User>>(getRepositoryToken(User));
+    userService = moduleFixture.get<UserService>(UserService);
     
     await app.init();
   });
 
-  it('debería crear un usuario correctamente', async () => {
+  // Prueba para create()
+  it('POST /users', async () => {
     // Mock del repositorio para asegurar que no existe un usuario con el mismo email
     jest.spyOn(userRepository, 'findOne').mockResolvedValue(null);
 
@@ -36,12 +46,35 @@ describe('Pruebas de humo para el módulo de usuario', () => {
       picture: 'http://example.com/picture.jpg'
     };
 
+    const expectedUser = {
+        id: 1,
+        firstName: 'Test',
+        lastName: 'User',
+        email: 'test@example.com',
+        picture: 'http://example.com/picture.jpg',
+    } as User;
+
+    jest.spyOn(userService, 'create').mockResolvedValue(expectedUser);
+
     const response = await request(app.getHttpServer())
       .post('/users')
       .send(userDto)
       .expect(201);
 
-    expect(response.body.email).toBe(userDto.email);
+    expect(response.body.email).toBe(expectedUser.email);
+  });
+
+  // Prueba para findAll()
+  it('GET /users', async () => {
+
+    jest.spyOn(userRepository, 'find').mockResolvedValue(mockedUsers);
+
+    const response = await request(app.getHttpServer())
+      .get('/users')
+      .expect(200);
+
+    expect(response.body).toEqual(mockedUsers); // Verifica que la respuesta sea igual a mockUsers
+    expect(response.body.length).toBe(2); // Verifica que hay dos usuarios
   });
 
   afterAll(async () => {
