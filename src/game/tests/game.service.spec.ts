5c1d96fea5f79983bca90c60097cad34817ee421
DanRuizK57 <d.ruiz03@ufromail.cl>
2024-10-30 21:15:26 -0300

    (test) Prueba unitaria de crear juego añadida

---
+++ src/game/tests/game.service.spec.ts
@@ -0,0 +1,148 @@
+import { Repository } from "typeorm";
+import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { GameService } from "../game.service";
+import { Game } from "../entities/game.entity";
+import { Type } from "../enums/type.enum";
+import { CreateGameDto } from "../dto/create-game.dto";
+import { User } from "src/user/entities/user.entity";
+import { Player } from "../entities/player.entity";
+
+/*
+  Pruebas unitarias para verificar el correcto funcionamiento de los métodos del servicio de partidas.
+*/
+describe('GameService', () => {
+  let service: GameService;
+  let gameRepository: Repository<Game>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        GameService,
+        {
+          provide: getRepositoryToken(Game),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(Player),
+          useClass: Repository,
+        },
+      ],
+    }).compile();
+
+    service = module.get<GameService>(GameService);
+    gameRepository = module.get<Repository<Game>>(getRepositoryToken(Game));
+
+    // Mock del método findAll
+    jest.spyOn(service, 'findAll').mockImplementation(async () => [
+      Object.assign(
+        new Game(), {
+        id: 1,
+        title: 'Partida 1',
+        description: "Partida",
+        duration: '30 mins',
+        date: '30/10/2024',
+        hour: '14:30',
+        latitude: '3232.234334',
+        longitude: '232.4324',
+        playerSlots: '4',
+        totalPlayers: '6',
+        type: Type.Type_1,
+        user: { id: 2, firstName: 'Jane', lastName: "Doe", email: 'jane.doe@example.com', picture: '' },
+        players: []
+      }
+      ),
+      Object.assign(new Game(), {
+        id: 2,
+        title: 'Partida 2',
+        description: "Partida 2",
+        duration: '20 mins',
+        date: '31/10/2024',
+        hour: '10:30',
+        latitude: '3232.234334',
+        longitude: '232.4324',
+        playerSlots: '4',
+        totalPlayers: '6',
+        type: Type.Type_1,
+        user: { id: 2, firstName: 'Jane', lastName: "Doe", email: 'jane.doe@example.com', picture: '' },
+        players: []
+      }),
+    ]);
+      
+    // Mock del método findOne
+    jest.spyOn(service, 'findOne').mockImplementation(async () =>
+      Object.assign(new Game(), {
+        id: 1,
+        title: 'Partida 1',
+        description: "Partida",
+        duration: '30 mins',
+        date: '30/10/2024',
+        hour: '14:30',
+        latitude: '3232.234334',
+        longitude: '232.4324',
+        playerSlots: '4',
+        totalPlayers: '6',
+        type: Type.Type_1,
+        user: { id: 2, firstName: 'Jane', lastName: "Doe", email: 'jane.doe@example.com', picture: '' },
+        players: []
+      })
+    );
+
+
+  });
+    
+  // ############################## Tests para create() ####################################################
+  describe('create', () => {
+
+    it('debería crear una partida', async () => {
+
+      const owner: User = {
+        id: 2,
+        firstName: 'Jane',
+        lastName: "Doe",
+        email: 'jane.doe@example.com',
+        picture: '',
+        role: 'user',
+        reports: 0,
+        games: [],
+        players: [],
+        createdAt: new Date()
+      }
+
+      const createGameDto: CreateGameDto = {
+        title: "Partida",
+        description: "Partida",
+        duration: "1 hora",
+        date: "28/10/2024",
+        hour: "17:30",
+        latitude: 223.324324,
+        longitude: 23432.234234,
+        playerSlots: 5,
+        totalPlayers: 10,
+        type: Type.Type_2
+      };
+
+      const createdGame = {
+        id: 1,
+        title: "Partida",
+        description: "Partida",
+        duration: "1 hora",
+        date: "28/10/2024",
+        hour: "17:30",
+        latitude: 223.324324,
+        longitude: 23432.234234,
+        playerSlots: 5,
+        totalPlayers: 10,
+        type: Type.Type_2
+      };
+
+      jest.spyOn(service, 'create').mockResolvedValue(createdGame as Game);
+
+      const result = await service.create(owner, createGameDto);
+      
+      expect(result).toEqual(createdGame);
+    });
+
+  });
+
+});
