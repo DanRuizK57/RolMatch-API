25fa098bd3308d95090fd896ac4fe792ecdf3b61
DanRuizK57 <d.ruiz03@ufromail.cl>
2024-11-01 16:39:39 -0300

    (tests) Tests para la función findAll() del controlador de partidas añadidos

--- src/game/tests/game.controller.spec.ts
+++ src/game/tests/game.controller.spec.ts
@@ -144,5 +144,66 @@ describe('GameController', () => {
 
     });
 
+    // ############################## Tests para findAll() ####################################################
+    describe('GET /games', () => {
+
+        it('debería retornar una lista de partidas', async () => { 
+
+            const result = await controller.findAll();
+
+            expect(result).toEqual([
+                Object.assign(
+                    new Game(), {
+                    id: 1,
+                    title: 'Partida',
+                    description: "Partida",
+                    duration: '30 mins',
+                    date: '30/10/2024',
+                    hour: '14:30',
+                    latitude: '3232.234334',
+                    longitude: '232.4324',
+                    playerSlots: '4',
+                    totalPlayers: '6',
+                    type: Type.Type_1,
+                    user: { id: 2, firstName: 'Jane', lastName: "Doe", email: 'jane.doe@example.com', picture: '' },
+                    players: []
+                }),
+                Object.assign(new Game(), {
+                    id: 2,
+                    title: 'Partida 2',
+                    description: "Partida 2",
+                    duration: '20 mins',
+                    date: '31/10/2024',
+                    hour: '10:30',
+                    latitude: '3232.234334',
+                    longitude: '232.4324',
+                    playerSlots: '4',
+                    totalPlayers: '6',
+                    type: Type.Type_1,
+                    user: { id: 2, firstName: 'Jane', lastName: "Doe", email: 'jane.doe@example.com', picture: '' },
+                    players: []
+                }),
+            ]);
+        });
+
+        it('hay 2 elementos en el array', async () => {
+            const result = await controller.findAll();
+            expect(result).toHaveLength(2);
+        });
+
+        it('todos los elementos de la lista deben ser instancias de Game', async () => {
+            const result = await controller.findAll();
+            result.forEach(game => {
+                expect(game).toBeInstanceOf(Game);
+            });
+        });
+        
+        it('debería retornar un array vacío si no hay partidas', async () => {
+            jest.spyOn(service, 'findAll').mockResolvedValueOnce([]);
+            const result = await controller.findAll();
+            expect(result).toEqual([]);
+        });
+        
+    });
     
 });
