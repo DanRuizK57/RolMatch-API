8d7b295da60720f902f447eff1a49e8c71aacc0b
DanRuizK57 <d.ruiz03@ufromail.cl>
2024-11-13 16:13:25 -0300

    Mitigación de vulnerbilidades encontradas en OWASP ZAP

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Habilitar CORS
-  app.enableCors();
+  app.enableCors({
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  // Activar Helmet para añadir encabezados de seguridad
+  app.use(helmet());
   
   await app.listen(3000);
 }
