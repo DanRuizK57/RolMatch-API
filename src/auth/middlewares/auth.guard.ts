eeaabcfb946bfe0af17f0d543eb858ce24b4bc89
DanRuizK57 <d.ruiz03@ufromail.cl>
2024-11-25 15:12:11 -0300

    (tests) Pruebas unitarias para el módulo de auth añadidas

--- src/auth/middlewares/auth.guard.ts
+++ src/auth/middlewares/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { AuthService } from './auth.service';
-import { UserService } from '../user/user.service';
+import { AuthService } from '../auth.service';
+import { UserService } from '../../user/user.service';
 
 /*
   Guardián encargado de verificar que la persona que quiera ingresar a una URL protegida
